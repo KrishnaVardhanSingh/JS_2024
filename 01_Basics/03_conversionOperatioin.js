a9b167b0eca49937cbc361181b707e4a08baad00
KrishnaVardhanSingh <101264076+KrishnaVardhanSingh@users.noreply.github.com>
2024-02-24 17:50:15 +0000

    conversion operation on basic dataType

---
+++ 01_Basics/03_conversionOperatioin.js
@@ -0,0 +1,39 @@
+let score
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let changeToNumber = Number(score)
+// console.log(typeof changeToNumber);
+// console.log(changeToNumber);
+
+/*
+    33 => 33
+    333dfad => NaN
+    asda => NaN
+    null = 0
+    true => 1, false => 0
+    undifeined => NaN
+*/
+
+
+let age = " "
+let changeToBool = Boolean(age)
+
+// console.log(changeToBool);
+
+/*
+    23 => true
+    0 => false
+    "" => false
+    " " => true
+*/
+
+let year = undefined
+let changeToString = String(year)
+
+console.log(changeToString);
+
+/*
+    we will get whatever we try to write in this variable year
+*/
