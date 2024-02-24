f375240e2e3f2267cc748f63864d116f98ebe381
KrishnaVardhanSingh <101264076+KrishnaVardhanSingh@users.noreply.github.com>
2024-02-24 18:47:31 +0000

    comparision operations - what NOT to do

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,18 @@
+// only compare same data type
+// == <= >= !=
+
+
+// bad practice
+
+console.log(null > 0);       // false
+console.log(null == 0);      // false
+console.log(null >= 0);      // true
+
+// >= work differently then > OR ==, it convert null into zer0
+
+
+
+
+// '===' -> it also check the data type of both the variables or inputs
+
+console.log("3" === 3);
