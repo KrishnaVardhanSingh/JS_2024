76cf52727ebffe5e084a9772fd84999fda59842d
KrishnaVardhanSingh <101264076+KrishnaVardhanSingh@users.noreply.github.com>
2024-02-25 04:54:40 +0000

    Heap and Stack

--- 01_Basics/05_dataTypeSummury.js
+++ 01_Basics/05_dataTypeSummury.js
@@ -29,3 +29,26 @@ let myFunction = function(){
 }
 
 console.log(myFunction);
+
+/***************************************** HEAP - STACK ********************************************/
+
+// HEAP (non-premivtive, get the reference of the value), STACK (premitive, get the copy of value)
+
+let varOne = "Krishna"
+let varTwo = varOne
+varTwo = "Vardhan"
+console.log(varOne, varTwo);
+
+let userOne = {
+    name : "krishna vardhan singh",
+    age : 21
+}
+let userTwo = userOne
+
+// this is a heap allocation and change the name of both the obj variables
+userTwo.name = "Kavindra Pratap Singh"
+console.log(userOne, userTwo);
+
+
+
+
