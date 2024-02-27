0231f9873985645e6e75ddf1fa19bb3ea5f18213
KrishnaVardhanSingh <101264076+KrishnaVardhanSingh@users.noreply.github.com>
2024-02-27 13:59:35 +0000

    basic loop same as c++

---
+++ 04_Control_flow/04_Iteration.js
@@ -0,0 +1,28 @@
+// // basic for loop
+for(let index = 1; index < 10; index++)
+{
+    console.log(index*index);
+}
+
+
+
+// // while loop
+
+let index = 1;
+while(index < 10)
+{
+    console.log(index * index);
+    index++
+}
+
+
+
+
+// do-while loop
+
+index = 1
+do{
+    console.log(`it will always print`);
+    console.log(index * index);
+    index++
+}while(index < 10);
