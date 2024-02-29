ceaa2cf10cba6763ebfbded972fdeeff3c1fdfaf
KrishnaVardhanSingh <101264076+KrishnaVardhanSingh@users.noreply.github.com>
2024-02-29 07:59:40 +0000

    update

--- 06_Project/05_set_unlimited_color/script.js
+++ 06_Project/05_set_unlimited_color/script.js
@@ -1,23 +1,36 @@
-const hex = "0123456789ABCDEF"
-    let colour = "#"
+const hex = "123456789ABCDEF"
+let colour = "#"
 
-    const changeClr = function(colour){
-        for(let i = 0; i < 6; i++)
-        {
-            colour += hex[Math.floor(Math.random() * 16)]
-        }
-        return colour
+// random colour generation
+function pgcolor(colour) {
+    for(let i = 0; i < 6; i++){
+        colour += hex[Math.floor((Math.random() * 16))] 
     }
-    
-    const start = function (){
-        document.body.style.backgroundColor = changeClr(colour)
-    }
-    
-    let initiate
-    document.querySelector("#start").addEventListener('click', function(){
-        initiate = setInterval(start, 1000)
-    })
+    return colour
+}
 
-    document.querySelector('#stop').addEventListener('click', function(){
-        clearInterval(initiate)
-    })
+
+const changeColor = function () {
+    document.body.style.backgroundColor = pgcolor(colour)
+}
+
+
+let controlVar
+const startChanging = function () {
+// check to privent prom multiple clicks
+    if(!controlVar){
+        controlVar = setInterval(changeColor, 1000)
+    }
+}
+
+
+const stopChanging = function () {
+    clearInterval(controlVar)
+    controlVar = null
+}
+
+
+document.querySelector("#start").addEventListener('click', startChanging)
+document.querySelector("#stop").addEventListener('click', stopChanging)
+
+
